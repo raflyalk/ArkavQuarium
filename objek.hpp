3639b31ea5d0e914cccc6558fc91f00b2740b14c
Aufa <muhammad.aufa.helfiandri@gmail.com>
2018-03-12 14:42:53 +0700

    ifndef

--- objek.hpp
+++ objek.hpp
@@ -1,1 +1,13 @@
-#include 
+#include <iostream>
+#include "aquarium.hpp"
+#include "Point.hpp"
+using namespace std;
+
+class Objek{
+    protected:
+        int price;
+    public:
+        Objek();
+        virtual move(Aquarium) = 0;
+
+}
