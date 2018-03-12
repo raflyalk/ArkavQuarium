556a2b81cd41adcb2769198643e6a3f2f7de148a
Aufa <muhammad.aufa.helfiandri@gmail.com>
2018-03-12 14:54:02 +0700

    disuruhRafli

--- aquarium.hpp
+++ aquarium.hpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "objek.hpp"
+
 
 class Aquarium{
     protected:
+        Objek** Matriks;
+        int MaxX, MaxY;
     public:
+        Aquarium();
+        Aquarium(int, int);
+        Aquarium(const Aquarium&);
+        ~Aquarium();
+               
+
 }
